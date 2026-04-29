2b4c15361e82d1f38268b0b77493eb70e9b7c223
Albina <garshanova.a@gmail.com>
2026-04-29 14:34:46 +0300

    Add files via upload

---
+++ Funnel Analysis 2026.sql
@@ -0,0 +1,151 @@
+SELECT *
+FROM `hjglkj.Funnel_Analysis.Funnel Analysis `
+LIMIT 1000;
+
+-- Define sales funnel and different stages
+
+WITH
+  funnel_stages AS (
+    SELECT
+      COUNT(DISTINCT CASE WHEN event_type = 'page_view' THEN user_id END)
+        AS stage_1_page_view,
+      COUNT(DISTINCT CASE WHEN event_type = 'add_to_cart' THEN user_id END)
+        AS stage_2_add_to_cart,
+      COUNT(DISTINCT CASE WHEN event_type = 'checkout_start' THEN user_id END)
+        AS stage_3_checkout_start,
+      COUNT(DISTINCT CASE WHEN event_type = 'payment_info' THEN user_id END)
+        AS stage_4_payment_info,
+      COUNT(DISTINCT CASE WHEN event_type = 'purchase' THEN user_id END)
+        AS stage_5_purchase
+    FROM `hjglkj.Funnel_Analysis.Funnel Analysis `
+    WHERE
+      event_date >= TIMESTAMP_SUB(
+        (SELECT MAX(event_date) FROM `hjglkj.Funnel_Analysis.Funnel Analysis `),
+        INTERVAL 30 DAY)
+  )
+SELECT *
+FROM funnel_stages;
+
+----conversion rates through the funnel
+
+WITH
+  funnel_stages AS (
+    SELECT
+      COUNT(DISTINCT CASE WHEN event_type = 'page_view' THEN user_id END)
+        AS stage_1_view,
+      COUNT(DISTINCT CASE WHEN event_type = 'add_to_cart' THEN user_id END)
+        AS stage_2_add_to_cart,
+      COUNT(DISTINCT CASE WHEN event_type = 'checkout_start' THEN user_id END)
+        AS stage_3_checkout,
+      COUNT(DISTINCT CASE WHEN event_type = 'payment_info' THEN user_id END)
+        AS stage_4_payment,
+      COUNT(DISTINCT CASE WHEN event_type = 'purchase' THEN user_id END)
+        AS stage_5_purchase
+    FROM `hjglkj.Funnel_Analysis.Funnel Analysis `
+    WHERE
+      event_date >= TIMESTAMP_SUB(
+        (SELECT MAX(event_date) FROM `hjglkj.Funnel_Analysis.Funnel Analysis `),
+        INTERVAL 30 DAY)
+  )
+SELECT
+  stage_1_view,
+  stage_2_add_to_cart,
+  round(stage_2_add_to_cart * 100 / stage_1_view) AS view_to_cart_rate,
+  stage_3_checkout,
+  round(stage_3_checkout * 100 / stage_2_add_to_cart) AS cart_to_checkout_rate,
+  stage_4_payment,
+  round(stage_4_payment * 100 / stage_3_checkout) AS checkout_to_payment_rate,
+  stage_5_purchase,
+  round(stage_5_purchase * 100 / stage_4_payment) AS payment_to_purchase_rate,
+  round(stage_5_purchase * 100 / stage_1_view) AS total_conversion_rate
+FROM funnel_stages;
+
+
+
+----- Funnel by source 
+
+WITH source_funnel as 
+( 
+SELECT
+  traffic_source, 
+  COUNT(DISTINCT CASE WHEN event_type = 'page_view' THEN user_id END) AS views,
+  COUNT(DISTINCT CASE WHEN event_type = 'add_to_cart' THEN user_id END) AS carts,
+  COUNT(DISTINCT CASE WHEN event_type = 'purchase' THEN user_id END) AS purchases
+FROM `hjglkj.Funnel_Analysis.Funnel Analysis `
+
+WHERE event_date >= TIMESTAMP_SUB(
+        (SELECT MAX(event_date) FROM `hjglkj.Funnel_Analysis.Funnel Analysis `),INTERVAL 30 DAY)
+GROUP BY traffic_source
+)
+
+SELECT 
+  traffic_source,
+  views,
+  carts,
+  purchases,
+  round(carts * 100 / views) AS cart_CR,
+  round(purchases * 100 / views) AS purchases_CR,
+  round(purchases * 100 / carts) AS cart_to_purchase_CR,
+
+FROM source_funnel
+ORDER BY purchases desc;
+
+
+
+---- Time to CR analysis
+
+WITH user_journey as 
+( 
+SELECT
+  user_id, 
+  MIN(CASE WHEN event_type = 'page_view' THEN event_date END) AS view_time,
+  MIN(CASE WHEN event_type = 'add_to_cart' THEN event_date END) AS cart_time,
+  MIN(CASE WHEN event_type = 'purchase' THEN event_date END) AS purchase_time
+FROM `hjglkj.Funnel_Analysis.Funnel Analysis `
+
+WHERE event_date >= TIMESTAMP_SUB(
+        (SELECT MAX(event_date) FROM `hjglkj.Funnel_Analysis.Funnel Analysis `),INTERVAL 30 DAY)
+GROUP BY   user_id
+HAVING MIN(CASE WHEN event_type = 'purchase' THEN event_date END) is not null
+)
+SELECT 
+  COUNT(*) as converted_users,
+  ROUND(AVG(TIMESTAMP_DIFF(cart_time, view_time, MINUTE)),2) as avg_view_to_cart_minutes,
+  ROUND(AVG(TIMESTAMP_DIFF(purchase_time, cart_time, MINUTE)),2) as avg_cart_to_purchase_minutes,
+  ROUND(AVG(TIMESTAMP_DIFF(purchase_time, view_time, MINUTE)),2) as avg_total_journey_minutes,
+FROM user_journey 
+
+
+--- Revenue Funnel Analysis
+
+WITH revenue_analysis as 
+( 
+SELECT
+  COUNT(DISTINCT CASE WHEN event_type = 'page_view' THEN user_id END) AS total_visitors,
+  COUNT(DISTINCT CASE WHEN event_type = 'purchase' THEN user_id END) AS total_buyers,
+  ROUND(SUM(CASE WHEN event_type = 'purchase' THEN amount END),2) AS total_revenue,
+  COUNT(CASE WHEN event_type = 'purchase' THEN 1 END) AS total_orders,
+FROM `hjglkj.Funnel_Analysis.Funnel Analysis `
+
+WHERE event_date >= TIMESTAMP_SUB(
+        (SELECT MAX(event_date) FROM `hjglkj.Funnel_Analysis.Funnel Analysis `),INTERVAL 30 DAY)
+)
+
+SELECT
+  total_visitors,
+  total_buyers,
+  total_revenue,
+  total_orders,
+  ROUND(SAFE_DIVIDE(total_revenue, total_orders), 2) AS avg_order_value,
+  ROUND(SAFE_DIVIDE(total_revenue, total_buyers), 2) AS revenue_per_buyer,
+  ROUND(SAFE_DIVIDE(total_revenue, total_visitors), 2) AS revenue_per_visitor
+
+FROM revenue_analysis;
+
+
+
+
+
+
+
+
